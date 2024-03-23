93bcfcecd63c2b0e9cc9735f16badb1eb103687d
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-23 21:29:44 +0000

    [feat]: fixed error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, INFUL(DATEDIFF(2022, formed), 0) AS lifespan
+SELECT band_name, COALESCE(split, 2022) formed AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style LIKE '%Glam rock'
 ORDER BY lifespan DESC;
