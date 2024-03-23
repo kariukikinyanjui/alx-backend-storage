f3e2b13ccba83be58e270289a32225d65c18ff54
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-23 21:09:25 +0000

    [feat]: import table dump

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Selects the country origin of bands and ranks them by the total number of fans
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
