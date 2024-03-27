e7f62d7443665b9e162d454db6d059af967a7068
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-27 02:02:41 +0000

    [feat]: lists all documents in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,4 +3,11 @@
 
 
 def list_all(mongo_collection):
+    """
+    Args:
+        mongo_collection: A Pymongo collection object
+
+    Returns:
+        list: A list containing all documents in the collection
+    """
     return [entry for entry in mongo_collection.find()]
